1e92414a19a8a27bee6c48347e2b4e65cda42061
alexikiphenix <alexikiphenix@gmail.com>
2022-07-03 05:50:02 +0200

    checkmail v4

--- regex.js
+++ regex.js
@@ -1,5 +1,8 @@
 "use strict";
+/*
+/w => mots, lettres : a-zA-Z_  (le tiret bas est inclu)
 
-const str = "Lo rem ipsum dolor sit amet";
-const regex = /(^lo)/gi;
+*/
+const str = "Lo4 lor12 _3";
+const regex = /\w/gi;  
 console.log(str.match(regex));
